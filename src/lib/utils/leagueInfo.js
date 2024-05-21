d71d9e6488c5e6a97302bbed6c9f47b7f2685284
Mason <105653530+mnewk41@users.noreply.github.com>
2024-05-21 10:20:40 -0400

    Update leagueInfo.js
    updated League ID

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "916418156338483200"; // your league ID
+export const leagueID = "1050115411863269376"; // your league ID
 export const leagueName = "317/812 Dynasty"; // your league name
 export const dues = 17; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
