947dba0f2c5908410d0927cda179ac5cd1ae45f6
Mason <105653530+mnewk41@users.noreply.github.com>
2024-05-21 10:00:47 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -92,10 +92,10 @@ export const managers = [
       "name": "Josh Avery",
       "tookOver": 2024, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Westport, IN", // (optional)
-      "bio": ,
-      "photo": , // square ratio recommended (no larger than 500x500)
-      "fantasyStart": , // (optional) when did the manager start playing fantasy football
-      "favoriteTeam": "", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "bio": win ,
+      "photo": null , // square ratio recommended (no larger than 500x500)
+      "fantasyStart":null  , // (optional) when did the manager start playing fantasy football
+      "favoriteTeam": "ind", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Rival", // Can be anything (usually your rival's name)
